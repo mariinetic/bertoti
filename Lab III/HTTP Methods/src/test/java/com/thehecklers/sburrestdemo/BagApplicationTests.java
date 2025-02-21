3c1b6e688b7c56a0060be4f1693c3b55cd3eaf7c
Mariana Oliveira <162724918+mariinetic@users.noreply.github.com>
2025-02-20 22:39:49 -0300

    using http methods
    fix the test class theme

--- Lab III/HTTP Methods/src/test/java/com/thehecklers/sburrestdemo/BagApplicationTests.java
+++ Lab III/HTTP Methods/src/test/java/com/thehecklers/sburrestdemo/BagApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SburRestDemoApplicationTests {
+class BagApplicationTests {
 
 	@Test
 	void contextLoads() {
